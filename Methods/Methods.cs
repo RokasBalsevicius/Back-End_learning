42af4dd2f1c57f281cec10beb7082d34e1b3bf8e
Rokas <rok.balse@gmail.com>
2025-10-26 00:20:10 +0300

    refactored TemperatureConverter() method

--- Methods/Methods.cs
+++ Methods/Methods.cs
@@ -11,30 +11,29 @@ public class LearningMethods
             Console.WriteLine("Choose the temperature format (type: F for Fahrenheit, C for Celsius)");
             string selectedFormat = Console.ReadLine()?.Trim().ToUpper();
 
-            if (selectedFormat == "F")
-            {
-                Console.WriteLine("Selected to convert temperature from Celsius to Fahrenheit");
-                Console.WriteLine("Enter the temperature");
-                double enteredTemperature = double.Parse(Console.ReadLine());
-                convertedTemp = enteredTemperature * 1.8 + 32;
-                Console.WriteLine($"Entered tempareture {enteredTemperature} in Celsius is {convertedTemp} in Fahrenheit");
-                break;
-            }
-            else if (selectedFormat == "C")
-            {
-                Console.WriteLine("Selected to convert temperature from Fahrenheit to Celsius");
-                Console.WriteLine("Enter the temperature");
-                double enteredTemperature = double.Parse(Console.ReadLine());
-                convertedTemp = (enteredTemperature - 32) / 1.8;
-                Console.WriteLine($"Entered tempareture {enteredTemperature} in Fahrenheit is {convertedTemp} in Celsius");
-                break;
-            }
-            else
+            if (selectedFormat != "F" && selectedFormat != "C")
             {
                 Console.WriteLine("Invalid format selected, try again...");
+                continue;
             }
+
+            Console.WriteLine($"Selected to convert temperature from {(selectedFormat == "F" ? "Celsius to Fahrenheit" : "Fahrenheit to Celsius")}");
+            Console.WriteLine("Enter the temperature");
+
+            if (!(double.TryParse(Console.ReadLine(), out double enteredTemperature)))
+            {
+                Console.WriteLine("Invalid number, try again...");
+                continue;
+            }
+
+            convertedTemp = selectedFormat == "F" ? enteredTemperature * 1.8 + 32 : (enteredTemperature - 32) / 1.8;
+
+            Console.WriteLine(
+                $"Entered temperature {enteredTemperature} in {(selectedFormat == "F" ? "Celsius" : "Fahrenheit")}" +
+                $"is {convertedTemp} in {(selectedFormat == "F" ? "Fahrenheit" : "Celsius")}");
+            break;
         }
-        return convertedTemp;
+        return Math.Round(convertedTemp, 1);
 
     }
 }

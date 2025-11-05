e7c51fe8be519ba4ae7638056d76ee5797d944fe
RokasBalsevicius <rok.balse@gmail.com>
2025-11-05 22:07:00 +0200

    Student gradebook feature
    * Start of Gradebook program, already created draft for adding a student and grades
    
    * completed remaining StudentGradebook method features - future refactoring is needed to make method clean

--- Methods/Methods.cs
+++ Methods/Methods.cs
@@ -362,4 +362,137 @@ public class LearningMethods
             Console.WriteLine($"Word '{item.Key}' repeats {item.Value} times");
     }
 
+    public void StudentGradebook()
+    {
+        Utility utils = new Utility();
+        Dictionary<string, List<decimal>> gradebook = new Dictionary<string, List<decimal>>
+        {
+            {"ROKAS", new List<decimal> {10, 4, 2, 5, 7, 8, 9}},
+            {"EMA", new List<decimal> {10, 9, 8, 10, 7, 8, 10}}
+        };
+
+        while (true)
+        {
+            Console.WriteLine("Gradebook Menu:");
+            Console.WriteLine("1 - Add student");
+            Console.WriteLine("2 - Add grade to a student");
+            Console.WriteLine("3 - Show average grade per student");
+            Console.WriteLine("4 - Show top-performing student");
+            Console.WriteLine("5 - Exit");
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>");
+
+            int userMenuChoice = utils.ReadInt("Enter menu option: ");
+
+            switch (userMenuChoice)
+            {
+                case 1:
+                    while (true)
+                    {
+                        Console.WriteLine("Adding a new student. Please enter student's name: ");
+                        string? newStudentName = Console.ReadLine()?.Trim().ToUpper();
+                        if (string.IsNullOrWhiteSpace(newStudentName))
+                        {
+                            Console.WriteLine("Student name cannot be null. Please enter a valid student name");
+                            continue;
+                        }
+                        if (!gradebook.ContainsKey(newStudentName))
+                        {
+                            gradebook[newStudentName] = new List<decimal> { };
+                            Console.WriteLine($"Added student: {newStudentName}");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Student name '{newStudentName}' already exists!");
+                            continue;
+                        }
+                    }
+                    continue;
+
+                case 2:
+                    while (true)
+                    {
+                        Console.WriteLine("Enter student name to add a grade: ");
+                        string? studentName = Console.ReadLine()?.Trim().ToUpper();
+                        if (!gradebook.ContainsKey(studentName) || string.IsNullOrWhiteSpace(studentName))
+                        {
+                            Console.WriteLine($"No student with entered name '{studentName}' exists or entered value is null");
+                            continue;
+                        }
+                        decimal grade;
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the grade you want to add or 'Exit' to return to main menu");
+                            string? input = Console.ReadLine()?.Trim().ToUpper();
+                            if (input == "EXIT")
+                            {
+                                Console.WriteLine("Exiting to main menu....");
+                                Console.WriteLine("-------------------");
+                                break;
+                            }
+                            if (!decimal.TryParse(input, out grade) || grade < 1 || grade > 10)
+                            {
+                                Console.WriteLine("Invalid number entered. Please enter grade between 1.0 and 10.0");
+                                continue;
+                            }
+                            else
+                            {
+                                List<decimal> grades = gradebook[studentName];
+                                grades.Add(Math.Round(grade, 1));
+                                Console.WriteLine($"Grade '{Math.Round(grade, 1)}' added succesfully. ");
+                                Console.WriteLine($"Current grades of student '{studentName}' are: " + string.Join(", ", grades));
+                                continue;
+                            }
+                        }
+                        break;
+                    }
+                    continue;
+
+                case 3:
+                    while (true)
+                    {
+
+                        Console.WriteLine("Currently existing names in gradebook: " + string.Join(", ", gradebook.Keys));
+                        Console.WriteLine("Enter student name you want to check average grade: ");
+                        string? name = Console.ReadLine()?.Trim().ToUpper();
+                        if (string.IsNullOrWhiteSpace(name))
+                        {
+                            Console.WriteLine("Student name cannot be null. Please enter a valid student name");
+                            continue;
+                        }
+                        if (!gradebook.ContainsKey(name))
+                        {
+                            Console.WriteLine($"Student with name '{name}' does not exist. Please enter another name for the below list");
+                            continue;
+                        }
+                        List<decimal> grades = gradebook[name];
+                        Console.WriteLine($"Average grade for student '{name}' is {Math.Round(grades.Average(), 1)}");
+                        break;
+                    }
+                    continue;
+                    
+                case 4:
+                    string topPerformer = null;
+                    decimal averageGrade = 0;
+
+                    foreach (var student in gradebook)
+                    {
+                        decimal oneStudentGrade = student.Value.Average();
+                        if (averageGrade < oneStudentGrade)
+                        {
+                            averageGrade = Math.Round(oneStudentGrade, 1);
+                            topPerformer = student.Key; 
+                        }
+                    }
+                    Console.WriteLine($"Top performer is student '{topPerformer}' with the average grade '{averageGrade}'.");                    
+                    continue;
+                case 5:
+                    Console.WriteLine("Exiting program...Bye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid menu option selected...");
+                    continue;
+            }
+        }
+    }
 }
